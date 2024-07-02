3a9ee4b1bbca74d85bbf5c8f6ac7e392b1409371
Dvir Ben Asuli <66518359+DBenAsuli@users.noreply.github.com>
2024-07-02 12:38:24 +0300

    Create RelyingParty.py

---
+++ RelyingParty.py
@@ -0,0 +1,3 @@
+# Advanced Topics in Online Privacy and Cybersecurity     Exercise 2
+# Dvir Ben Asuli                                          318208816
+# The Hebrew University of Jerusalem                      June 2024
